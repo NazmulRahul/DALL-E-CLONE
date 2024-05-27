32f1a9a64ef28ea751707b731dedda710eb079c7
Nazmul Hossen Rahul <69730793+nazmul-2019331047@users.noreply.github.com>
2024-05-28 00:03:15 +0600

    client set up

---
+++ Client/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router,Routes,Route,Link } from "react-router-dom"
+export default function App() {
+  return (
+
+    <>
+    <Router>
+      <Routes>
+
+      </Routes>
+    </Router>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </>
+   
+  )
+}
